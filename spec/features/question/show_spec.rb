68918ea187c882918e5f131a0d509cb77a83bcef
Anna Kan <Annak1794@gmail.com>
2019-09-22 22:58:14 +0300

    completed user can asnwer questions user story

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -5,10 +5,11 @@ feature 'User can answer questions', %q{
   As an authenticated user
   I'd like to post an answer from question's page
 } do
+  given(:question) { create(:question) }
 
   describe 'Authenticated user' do
     scenario 'posts an answer to the question' do
-      question = create(:question)
+      sign_in(create(:user))
       visit question_path(question)
       fill_in 'Your answer', with: 'Test answer'
       click_on 'Submit'
@@ -18,7 +19,11 @@ feature 'User can answer questions', %q{
   end
 
   describe 'Unauthenticated user' do
-
+    scenario 'sees a warning message instead of an answer form' do
+      visit question_path(question)
+      expect(page).not_to have_content 'Your answer'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
   end
 
 end
