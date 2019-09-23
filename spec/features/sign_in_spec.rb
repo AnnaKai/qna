48eb38092e447b6ea38a7955459ce2992e854451
Anna Kan <Annak1794@gmail.com>
2019-09-24 00:01:36 +0300

    added user sign up story

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,9 +11,7 @@ feature 'User can sign in', %q{
   background { visit new_user_session_path }
 
   scenario 'Registered user tries to sign in' do
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    sign_in(user)
 
     expect(page).to have_content 'Signed in successfully.'
   end
@@ -21,7 +19,7 @@ feature 'User can sign in', %q{
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@email.com'
     fill_in 'Password', with: '123456'
-    click_on 'Log in'
+    click_button 'Log in'
 
     expect(page).to have_content 'Invalid Email or password.'
   end
