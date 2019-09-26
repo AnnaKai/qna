3e411ee0d550e5200e4690386e209ad45f9112bb
Anna Kan <Annak1794@gmail.com>
2019-09-26 23:50:04 +0300

    updated answer controller test with unauthenticated user create

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,28 +4,38 @@ RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
     let(:question) { create(:question) }
     let(:body) { Faker::Lorem.paragraph }
-    let(:user) { create(:user) }
-    before { sign_in(user) }
 
-    context 'with valid attributes' do
-      it 'saves a new answer in the db' do
-        expect { post :create, params: { question_id: question.id, body: body } }.to change { question.answers.count }.by(1)
-        expect(Answer.last).to have_attributes(body: body, user_id: user.id)
+    context 'authenticated user' do
+      let(:user) { create(:user) }
+      before { sign_in(user) }
+
+      context 'with valid attributes' do
+        it 'saves a new answer in the db' do
+          expect { post :create, params: { question_id: question.id, body: body } }.to change { question.answers.count }.by(1)
+          expect(Answer.last).to have_attributes(body: body, user_id: user.id)
+        end
+
+        it 'redirects to show view' do
+          post :create, params: { question_id: question.id, body: body }
+          expect(response).to redirect_to(question)
+        end
       end
-
-      it 'redirects to show view' do
-        post :create, params: { question_id: question.id, body: body }
-        expect(response).to redirect_to(question)
+      context 'with invalid attributes' do
+        it 'does not save the answer' do
+          expect { post :create, params: { question_id: question.id, body: nil } }.to_not change { Answer.count }
+        end
+        it 're-renders question view' do
+          post :create, params: { question_id: question.id, body: nil }
+          expect(response).to render_template 'questions/show'
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not save the answer' do
-        expect { post :create, params: { question_id: question.id, body: nil } }.to_not change { Answer.count }
-      end
-      it 're-renders question view' do
-        post :create, params: { question_id: question.id, body: nil }
-        expect(response).to render_template 'questions/show'
+    context 'unauthenticated user' do
+      it 'can not post an answer' do
+        post :create, params: { question_id: question.id, body: body }
+        expect(Answer.count).to eq(0)
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
