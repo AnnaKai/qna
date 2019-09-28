9d80e5e817d7b2ac7cbec539dddec824b31f88e6
AnnaKai <Annak1794@gmail.com>
2019-09-28 17:22:13 +0300

    added #destroy test for answers controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+
   describe 'POST #create' do
     let(:question) { create(:question) }
     let(:body) { Faker::Lorem.paragraph }
@@ -14,7 +15,7 @@ RSpec.describe AnswersController, type: :controller do
           expect { post :create, params: { question_id: question.id, body: body } }.to change { question.answers.count }.by(1)
         end
 
-        it 'saved answer has correct values' do
+        it 'saved answer has correct values including an author' do
           post :create, params: { question_id: question.id, body: body }
           expect(Answer.last).to have_attributes(body: body, user_id: user.id)
         end
@@ -29,6 +30,7 @@ RSpec.describe AnswersController, type: :controller do
         it 'does not save the answer' do
           expect { post :create, params: { question_id: question.id, body: nil } }.to_not change { Answer.count }
         end
+
         it 're-renders question view' do
           post :create, params: { question_id: question.id, body: nil }
           expect(response).to render_template 'questions/show'
@@ -42,6 +44,48 @@ RSpec.describe AnswersController, type: :controller do
       it 'does not save an answer' do
         expect(Answer.count).to eq(0)
       end
+
+      it 'gets asked to authorize' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question, author: user) }
+    let(:user) { create(:user) }
+
+    context 'authenticated' do
+      context 'author' do
+        before { sign_in(user) }
+
+        it 'deletes their answer in the db' do
+          expect { delete :destroy, params: { question_id: question.id, id: answer.id } }.to change(Answer, :count).by(-1)
+        end
+
+        it 'gets redirected to the initial question' do
+          delete :destroy, params: { question_id: question.id, id: answer.id }
+          expect(response).to redirect_to question
+        end
+      end
+
+      context 'not an author' do
+        before { sign_in(create(:user)) }
+
+        it 'does not delete the answer' do
+          expect { delete :destroy, params: { question_id: question.id, id: answer.id } }.to_not change(Answer, :count)
+        end
+
+        it 're-renders the answer\'s question' do
+          delete :destroy, params: { question_id: question.id, id: answer.id }
+          expect(response).to redirect_to question
+        end
+      end
+    end
+
+    context 'unauthenticated' do
+      before { delete :destroy, params: { question_id: question.id, id: answer.id } }
 
       it 'gets asked to authorize' do
         expect(response).to redirect_to(new_user_session_path)
