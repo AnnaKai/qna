b42296d5ee7687785faa78e5de7ef4f9ee30698a
AnnaKai <Annak1794@gmail.com>
2019-09-28 22:23:45 +0300

    user model tests for author

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:questions) }
-  it { should have_many(:answers) }
-  
+  it { should have_many(:questions).dependent(:destroy) }
+  it { should have_many(:answers).dependent(:destroy) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'question author' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, author: user) }
+
+    it 'user is the author' do
+      expect(user).to be_author_of(question)
+    end
+
+    it 'user is not the author' do
+      expect(create(:user)).not_to be_author_of(question)
+    end
+  end
+
+  describe 'answer author' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer, question_id: question.id, author: user) }
+
+    it 'user is the author' do
+      expect(user).to be_author_of(answer)
+    end
+
+    it 'user is not the author' do
+      expect(create(:user)).not_to be_author_of(answer)
+    end
+  end
 end
