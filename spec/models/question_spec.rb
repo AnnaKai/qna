5c2e51d6a3c291664156eecca0d95793fa96eae1
Anna Kan <Annak1794@gmail.com>
2019-10-20 16:07:48 +0300

    added sorting for question answers by best

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :body }
 
   it { should belong_to(:best_answer).optional }
+
+  describe '#sorted_answers' do
+    it 'the best answer is the first' do
+      question = create(:question)
+      answer = create(:answer, question: question)
+      best_answer = create(:answer, question: question)
+      question.update(best_answer: best_answer)
+
+      expect(question.sorted_answers.all.to_a).to eq([best_answer, answer])
+    end
+  end
 end
