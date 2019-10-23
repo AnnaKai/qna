1c553b1283fe663cdde240c687cb1454925b6392
Anna Kan <Annak1794@gmail.com>
2019-10-24 01:06:58 +0300

    Avoided N+1 queries when using Answer#best?

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :user_id
-  has_many :answers, -> (question) { question.best_answer_id ? order("id = #{question.best_answer_id} desc") : all }, dependent: :destroy
+  has_many :answers, -> (question) { question.best_answer_id ? preload(:question).order("id = #{question.best_answer_id} desc") : preload(:question).all }, dependent: :destroy
   has_a :best_answer, class_name: "Answer", optional: true
 
   validates :body, :title, presence: true
