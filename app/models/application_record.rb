5c0a45cd934709c91978d95be00f7059b8a7e891
Anna Kan <Annak1794@gmail.com>
2019-10-23 22:03:12 +0300

    added alias for belongs_to to reflect the real association that one object might have to another

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  class << self
+    alias has_a belongs_to
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
-  has_many :answers, dependent: :destroy
+  has_many :answers, dependent: :destroy # TODO: custom scope block with ordering
   belongs_to :author, class_name: "User", foreign_key: :user_id
+  has_a :best_answer, class_name: "Answer", optional: true
   validates :body, :title, presence: true
 
   def sorted_answers
