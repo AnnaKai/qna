76868e267e2c8ed76ae0247a317b8a0a59a887c0
Anna Kan <Annak1794@gmail.com>
2019-10-24 02:12:16 +0300

    Removed argument from the answers scope for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :user_id
-  has_many :answers, -> (question) { question.best_answer_id ? preload(:question).order("id = #{question.best_answer_id} desc") : preload(:question).all }, dependent: :destroy
+  has_many :answers, -> { preload(:question).joins(:question).select('answers.*, questions.best_answer_id').order('answers.id = questions.best_answer_id desc') }, dependent: :destroy
   has_a :best_answer, class_name: "Answer", optional: true
 
   validates :body, :title, presence: true
