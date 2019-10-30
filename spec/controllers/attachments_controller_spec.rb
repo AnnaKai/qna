ab8ad019cb57ec6a228e0d47b26a9b3605dee1ed
Anna Kan <Annak1794@gmail.com>
2019-10-30 15:49:52 +0300

    added tests for editing question with attachments

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
-  describe 'DELETE #destroy' do
-    before { login(user) }
+
+ describe 'DELETE #destroy' do
     let(:file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb") }
-    let(:question) { create(:question, user: user, files: [file]) }
+    let(:question) { create(:question, files: [file]) }
 
     context 'authenticated user' do
+      before { login(question.author) }
+
       context 'is author' do
         it 'deletes attached files' do
-
+          expect { delete :destroy, params: { id: question.files.first }, format: :js}.to change(question.files, :count).by(-1)
         end
 
         it 'renders destroy view' do
