2490527f0e343ec2b91787f36f281041e5ba44dc
Anna Kan <Annak1794@gmail.com>
2019-11-04 19:15:34 +0100

    fixed tests; added attachments controller and s3 credenials

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -31,55 +31,59 @@ feature 'User can edit their question', %q{
         expect(page).to_not have_content question.body
         expect(page).to_not have_selector 'textarea'
       end
-
-      context 'edits their question with attached files' do
-        background do
-          within '.question' do
-            click_on 'Edit'
-            attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-            click_on 'Save'
-          end
-        end
-
-        scenario 'adds files' do
-          expect(page).to have_link 'rails_helper.rb'
-          expect(page).to have_link 'spec_helper.rb'
-        end
-
-        scenario 'deletes files' do
-          within('.question') do
-            first('.file').click_on 'Delete'
-          end
-
-          expect(page).to_not have_link 'rails_helper.rb'
-          expect(page).to have_link 'spec_helper.rb'
-        end
-      end
     end
 
-    scenario 'author edits their question with errors', js: true do
-      sign_in(question.author)
-      visit question_path(question)
+    context 'author edits their question with attached files', js: true do
+      background do
+        sign_in(question.author)
+        visit question_path(question)
 
-      within '.question' do
-        click_on 'Edit'
-        fill_in 'Title', with: ''
-        fill_in 'Body', with: ''
-        click_on 'Update'
-        expect(page).to have_content question.body
-        expect(page).to have_selector 'textarea'
+        within '.question' do
+          click_on 'Edit'
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Update'
+        end
       end
-      expect(page).to have_content 'Title can\'t be blank'
-      expect(page).to have_content 'Body can\'t be blank'
-    end
 
-    scenario 'not an author can not edit other users\' questions' do
-      sign_in(not_author)
-      visit question_path(question)
+      scenario 'adds files' do
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
 
-      within '.question' do
-        expect(page).to_not have_content 'Edit'
+      scenario 'deletes files' do
+        within('.question') do
+          first('.file').click_on 'Delete Attachment'
+        end
+
+        expect(page).to_not have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
       end
     end
   end
+
+  scenario 'author edits their question with errors', js: true do
+    sign_in(question.author)
+    visit question_path(question)
+
+    within '.question' do
+      click_on 'Edit'
+      fill_in 'Title', with: ''
+      fill_in 'Body', with: ''
+      click_on 'Update'
+      expect(page).to have_content question.body
+      expect(page).to have_selector 'textarea'
+    end
+    expect(page).to have_content 'Title can\'t be blank'
+    expect(page).to have_content 'Body can\'t be blank'
+  end
+
+  scenario 'not an author can not edit other users\' questions' do
+    sign_in(not_author)
+    visit question_path(question)
+
+    within '.question' do
+      expect(page).to_not have_content 'Edit'
+    end
+  end
 end
+
