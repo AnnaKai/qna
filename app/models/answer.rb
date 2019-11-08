4e88918c7f8e32cdb6d9c7eb5d1d8bf362ab6313
AnnaKai <Annak1794@gmail.com>
2019-11-09 00:34:33 +0100

    added links for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :author, class_name: "User", foreign_key: :user_id
-  validates :body, presence: true
+  has_many :links, dependent: :destroy, as: :linkable
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank
+
+  validates :body, presence: true
 
   default_scope { order(best: :desc) }
 
