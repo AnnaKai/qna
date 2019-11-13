9da39cfe95964243aa640e7a0c464d8f3aecdeb2
AnnaKai <Annak1794@gmail.com>
2019-11-13 03:12:29 +0100

    added user deletes answer's links feature spec

---
+++ spec/features/answer/destroy_links_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can delete links', %q{
+  In order to remove outdated links
+  As an author
+  I'd like to be able to delete links
+} do
+
+  given(:user) { create(:user) }
+  given(:answer) { create(:answer, author: user) }
+  given!(:link) { create(:link, linkable: answer) }
+
+  scenario 'user deletes a link when editing an answer', js: true do
+    sign_in(answer.author)
+
+    visit question_path(answer.question)
+
+    within first('.answer') do
+      click_on 'Edit'
+      
+      within first('.link-fields') do
+        click_on 'Remove link'
+      end
+
+      click_on 'Submit'
+    end
+
+    expect(page).to_not have_link link.name, href: link.url
+  end
+end
