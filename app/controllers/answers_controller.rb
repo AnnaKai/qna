fefecd94dc5074f643170b691f0a5f5cd9d1ff61
AnnaKai <Annak1794@gmail.com>
2019-11-13 02:50:42 +0100

    updated answers' model and controller to include attributes for links; the view file has been refactored

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -35,7 +35,7 @@ class AnswersController < ApplicationController
   helper_method :answer, :question
 
   def answer_params
-    params.require(:answer).permit(:body, files: [], links_attributes: [:name, :url])
+    params.require(:answer).permit(:body, files: [], links_attributes: [:id, :name, :url,  :_destroy])
   end
 
 end
