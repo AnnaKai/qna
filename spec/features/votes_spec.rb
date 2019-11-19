6ce7494d74f4c4372e740a2327014b9b2c3b551b
AnnaKai <Annak1794@gmail.com>
2019-11-19 03:36:23 +0100

    added votes feature spec

--- spec/features/votes_spec.rb
+++ spec/features/votes_spec.rb
@@ -6,11 +6,19 @@ feature 'User can vote for questions/answers', %q(
   I'd like to be able to increase/decrease its rating
 ) do
 
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
 
   describe 'authenticated user', js: true do
     context 'voting for others\' questions' do
       scenario 'votes for the question they like' do
+        sign_in(user)
+        visit question_path(question)
+        expect(page.find('.rating')).to have_content '0'
+        click_on 'Vote up'
+        expect(page.find('.rating')).to have_content '1'
+        visit question_path(question)
+        expect(page.find('.rating')).to have_content '1'
       end
 
       scenario 'changes their vote' do
@@ -28,16 +36,16 @@ feature 'User can vote for questions/answers', %q(
 
     scenario 'cannot see voting options for their own question' do
       sign_in(question.author)
-      expect(page).to_not have_selector(:link_or_button, 'Upvote')
-      expect(page).to_not have_selector(:link_or_button, 'Downvote')
+      expect(page).to_not have_selector(:link_or_button, 'Vote up')
+      expect(page).to_not have_selector(:link_or_button, 'Vote down')
     end
   end
 
   describe 'unauthenticated user', js: true do
     scenario 'cannot see voting options' do
       visit question_path(question)
-      expect(page).to_not have_selector(:link_or_button, 'Upvote')
-      expect(page).to_not have_selector(:link_or_button, 'Downvote')
+      expect(page).to_not have_selector(:link_or_button, 'Vote up')
+      expect(page).to_not have_selector(:link_or_button, 'Vote down')
     end
   end
 
